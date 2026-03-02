7876057eb5f708f468800fb588b5068299e65d39
AndriiShurhai <150887721+AndriiShurhai@users.noreply.github.com>
2026-03-02 18:41:29 +0200

    Getting to know vectors finished

---
+++ Assets/Move.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    public GameObject goal;
+    public float speed = 1f;
+
+    private Vector3 direction;
+    void Start()
+    {
+        direction = goal.transform.position - transform.position;
+    }
+    private void Update()
+    {
+        direction = goal.transform.position - transform.position;
+        transform.LookAt(goal.transform.position);
+        if (direction.magnitude > 0.1f)
+        {
+            Vector3 velocity = direction.normalized * speed * Time.deltaTime;
+            transform.position = transform.position + velocity;
+        }
+    }
+}
